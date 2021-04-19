590023c3a2196ddbcbeba996cdf63a817a8e971d
seanschumacher <seanschumacher10@gmail.com>
2021-04-18 21:04:03 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Aviation App
+# Created by Sean Schumacher
+# 2021
+
+user = 'sean'
+password = '1234'
+
+def Login():
+    print('Welcome to the Aviation App')
+    print('Enter usename and password below')
